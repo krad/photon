6aa781c61e4a53d2270ea2da4d37ec1733cbd5d2
krad <iam@krad.io>
2018-02-26 15:39:09 -0600

    Adding the login call to the main photon app thingie

--- Sources/photon/photon.swift
+++ Sources/photon/photon.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+public typealias LoginCallback = (User?, Error?) -> Void
+
 public protocol PhotonProtocol {
     func startBroadcast(name title: String, onReady: @escaping BroadcastBeginCallback)
 }
@@ -7,9 +9,28 @@ public protocol PhotonProtocol {
 public class Photon: PhotonProtocol {
     
     internal var webClient: PhotonWebAPI
+    internal var currentUser: User?
     
     public init(_ host: String) {
         self.webClient = PhotonWebAPI(host: host)
+    }
+    
+    public func login(countryCode: CountryCode,
+                      phoneNumber: String,
+                      password: String,
+                      onComplete: @escaping LoginCallback)
+    {
+        let request = PhoneLogin(countryCode: countryCode, phoneNumber: phoneNumber, password: password)
+        self.webClient.send(request) { (result) in
+            switch result {
+            case .success(let user):
+                self.currentUser = user
+                onComplete(user, nil)
+            case .failure(let error):
+                self.currentUser = nil
+                onComplete(nil, error)
+            }
+        }
     }
     
     public func startBroadcast(name title: String, onReady: @escaping BroadcastBeginCallback) {
